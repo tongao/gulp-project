f25f0ac89234a7c2fc33718c49d37d40e8ede879
Tong <header@163.com>
2015-05-18 14:44:18 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -24,56 +24,62 @@ var gulp = require('gulp'),
 
   	sprite = require('css-sprite').stream, //css雪碧图  https://www.npmjs.com/package/css-sprite
 
-    jade = require('jade'),
+    jade = require('jade'),   //jade
 
-    gulpJade = require('gulp-jade'),
+    gulpJade = require('gulp-jade'), //gulp-jade
 
     //katex = require('katex'),
     
-    data = require('gulp-data'),
+    data = require('gulp-data'), //gulp-data
 
     path = require('path'),
 
-    reload = browserSync.reload;
+    reload = browserSync.reload,
+
+    dir = "web",
+
+    markDir = "./web";
+
+
 
 
 //启动webServer
 gulp.task('serve', ['jade','sass', 'html', 'sprites', 'picmin', 'js'], function() {
 
     browserSync.init({
-        server: "./web"
+        server: markDir
     });
-    gulp.watch("web/src/tpl/*.jade", ['jade']);
-    gulp.watch("web/src/scss/*.scss", ['sass']);
-    gulp.watch("web/src/images/sprite/*", ['sprites']);
-    gulp.watch("web/src/images/*", ['picmin']);
+    gulp.watch(dir+"/src/tpl/*.jade", ['jade']);
+    gulp.watch(dir+"/src/scss/*.scss", ['sass']);
+    gulp.watch(dir+"/src/images/sprite/*", ['sprites']);
+    gulp.watch(dir+"/src/images/*", ['picmin']);
     //gulp.watch("web/src/*.html", ['html']);
-    gulp.watch("web/src/js/*.js", ['js']);
-    gulp.watch("web/dist/*.html").on('change', reload);
-    gulp.watch("web/dist/css/*.css").on('change', reload);
-    gulp.watch("web/dist/js/*.js").on('change', reload);
+    gulp.watch(dir+"/src/js/*.js", ['js']);
+    gulp.watch(dir+"/dist/*.html").on('change', reload);
+    gulp.watch(dir+"/dist/css/*.css").on('change', reload);
+    gulp.watch(dir+"/dist/js/*.js").on('change', reload);
 });
 //另存html页面
 gulp.task('html', function () {
-  return gulp.src('./web/src/*.html')
-    .pipe(gulp.dest('./web/dist/'));
+  return gulp.src(markDir+'/src/*.html')
+    .pipe(gulp.dest(markDir+'/dist/'));
 });
 //编译SASS
 gulp.task('sass', function() {
-    return sass('./web/src/scss/', { sourcemap: true, style: 'expanded' })
+    return sass(markDir+'/src/scss/', { sourcemap: true, style: 'expanded' })
     .pipe(sourcemaps.write())  //sass 调试Map
     .pipe(autoprefixer({
            browsers: ['last 2 versions'],
            cascade: false
     }))
     //.pipe(concat('all.css'))  //样式合并
-    .pipe(gulp.dest('./web/dist/css/'))
+    .pipe(gulp.dest(markDir+'/dist/css/'))
 });
 
 //css雪碧图
 gulp.task('sprites', function () {
 
-  return gulp.src('./web/src/images/sprite/*.png')
+  return gulp.src(markDir+'/src/images/sprite/*.png')
 
     .pipe(sprite({
       name: 'sprite',
@@ -82,27 +88,27 @@ gulp.task('sprites', function () {
       processor: 'scss'
     }))
     
-    .pipe(gulpif('*.png', gulp.dest('./web/dist/images/sprite/'), gulp.dest('./web/src/scss/')));
+    .pipe(gulpif('*.png', gulp.dest(markDir+'/dist/images/sprite/'), gulp.dest(markDir+'/src/scss/')));
 });
 
 //图标压缩
 gulp.task('picmin', function(){
 
-  return gulp.src('./web/src/images/*')
+  return gulp.src(markDir+'/src/images/*')
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         //use: [pngquant()]
     }))
-    .pipe(gulp.dest('./web/dist/images'));
+    .pipe(gulp.dest(markDir+'/dist/images'));
 });
 
 //js验证
 gulp.task('js', function(){
-	return gulp.src('./web/src/js/*.js')
+	return gulp.src(markDir+'/src/js/*.js')
 			   .pipe(jshint())
 			   .pipe(jshint.reporter('default'))
-			   .pipe(gulp.dest('./web/dist/js/'));
+			   .pipe(gulp.dest(markDir+'/dist/js/'));
 });
 
 //jade编译
@@ -111,20 +117,20 @@ gulp.task('js', function(){
 //  return str + '!!!!';
 //}
 gulp.task('jade', function () {
-  return gulp.src('./web/src/tpl/*.jade')
+  return gulp.src(markDir+'/src/tpl/*.jade')
     .pipe(data(function(file) {
-      return require('./web/src/data/' + path.basename(file.path) + '.json');
+      return require(markDir+'/src/data/' + path.basename(file.path) + '.json');
     }))
     .pipe(gulpJade({
       jade: jade,
       pretty: true
     }))
-    .pipe(gulp.dest('./web/dist/'))
+    .pipe(gulp.dest(markDir+'/dist/'))
 })
 
 //清空文件夹 
 gulp.task('clean', function(){
-	return gulp.src(['./web/dist/css', './web/dist/js', './web/dist/images', './web/dist/*.html'], {read: false})
+	return gulp.src([markDir+'/dist/css', markDir+'/dist/js', markDir+'/dist/images', markDir+'/dist/*.html'], {read: false})
 		.pipe(clean());
 });
 
